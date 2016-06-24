64093cfe392629859a474ec612a0867b03ea1eea
loriele <looriele@gmail.com>
2016-06-23 21:26:05 -0300

    Primeiro envio

---
+++ CaixaEletronico/src/Caixa.java
@@ -0,0 +1,34 @@
+
+public class Caixa {
+  int saque = 784;
+  int cem = 0, cinquenta = 0, dez = 0, cinco = 0, dois = 0;
+  int temp;
+  
+   temp = saque;
+ 
+ if(temp >= 100){
+   cem = saque / 100;
+   temp = saque % 100;
+ }
+
+ if(temp >= 50){
+   cinquenta = saque / 50;
+   temp = temp % 50;
+ }
+ 
+ if(temp >= 10){
+   dez = saque / 10;
+   temp = temp % 10;
+ }
+
+ if(temp >= 5){
+   cinco = saque / 5;
+   temp = temp % 5;
+ }
+ 
+ if(temp >= 1){
+  dois = saque / 2;
+ }
+  
+  }
+}
---
+++ CaixaEletronico/src/CaixaEletronicos.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class CaixaEletronicos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner CixaEletronico = new Scanner(System.in);
+      System.out.print("Informe o saque:");
+      int temp = saque 
+      
+      
+	}
+
+}
